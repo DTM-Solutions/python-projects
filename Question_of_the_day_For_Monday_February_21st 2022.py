e7621c6ea80fddf120744c4150bdcafb7e61f6e0
Milua25 <88958771+Milua25@users.noreply.github.com>
2022-02-21 20:24:14 -0500

    Create Question_of_the_day_For_Monday_February_21st 2022.py

---
+++ Question_of_the_day_For_Monday_February_21st 2022.py
@@ -0,0 +1,35 @@
+# Question 1: 
+def calculate (num1, num2=4):
+  res = num1 * num2
+  print(res)
+
+calculate(5, 6)
+
+# result = 30
+
+# Question 2.
+var = "James" * 2  * 3
+print(var)
+
+# result = JamesJamesJamesJamesJamesJames
+
+# Question 3.
+x = 36 / 4 * (3 +  2) * 4 + 2
+print(x)
+
+# result = 182.0
+
+## Question 4.
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+# result = unsupported operand type(s) for +: 'int' and 'str'
+
+## Question 5.
+p, q, r = 10, 20 ,30
+print(p, q, r)
+
+# result = 10 20 30
